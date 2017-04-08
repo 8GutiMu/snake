9ee0cf9da80bd03c49061da751671ba4801585b4
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-04-08 15:58:52 -0500

    funcion crear terminada, funcion mover definida

--- assets/main.js
+++ assets/main.js
@@ -1,17 +1,19 @@
-var btnCreate= document.getElementById('enviar');
+var btnCreate= document.getElementById('submit');
+
 
 btnCreate.addEventListener("click",createNewGame);
+document.addEventListener("keydown",moveSnake);
 
 function createNewGame(){
   var container = document.getElementById('container');
   var backgroundSnake = document.getElementById("edge");
   var snake = document.getElementById("snake");
-  var tamanioGame = tamanioTabla();
+  var sizeGame = sizeTable();
 
-  tamanioGame = tamanioGame*10;
+  sizeGame = sizeGame*10;
 
-  backgroundSnake.style.width = tamanioGame + "px";
-  backgroundSnake.style.height = tamanioGame + "px";
+  backgroundSnake.style.width = sizeGame + "px";
+  backgroundSnake.style.height = sizeGame + "px";
   backgroundSnake.style.backgroundColor = "green";
   backgroundSnake.style.position = "absolute";
 
@@ -21,13 +23,62 @@ function createNewGame(){
   snake.style.backgroundColor = "black";
   snake.style.position = "absolute";
 
+
   container.appendChild(backgroundSnake);
   container.appendChild(snake);
 
 }
 
-function tamanioTabla(){
-  var tamanio = document.getElementById('tamanio').value;
-  return tamanio;
+function sizeTable(){
+  var size = document.getElementById('sizeInput').value;
+  return size;
 
+}
+
+function moveSnake(e){
+    var limite = sizeTable();
+    limite = limite*10;
+    limite -= 10;
+
+
+    var situacionY = document.getElementById("snake").offsetLeft;
+    var situacionX = document.getElementById("snake").offsetTop;
+
+       switch (e.keyCode){
+           case  37:
+             if (situacionY >0) {
+               snake.style.left = situacionY-10+"px" ;
+             }else {
+               youLost();
+             }
+
+               break;
+           case 38:
+           if (situacionX >0) {
+               snake.style.top = situacionX-10+"px" ;
+           }else {
+             youLost();
+           }
+               break;
+
+           case 39 :
+           if (situacionY < limite) {
+               snake.style.left = situacionY+10+"px" ;
+           }else{
+               youLost();
+           }
+
+               break;
+           case 40 :
+           if(situacionX < limite){
+             snake.style.top = situacionX+10+"px" ;
+           }else{
+             youLost()
+           }
+           break;
+}
+}
+
+function youLost(){
+  alert("PERDISTE");
 }
