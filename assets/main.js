b6258525d83bbc4c3265b8ab1a50a6071380a8ff
Brenda gutierrez murguia <Brenda gutierrez murguia>
2017-04-08 14:31:15 -0500

    declaracion de variables buenas, creacion de funcion de crear tablagit add .

--- assets/main.js
+++ assets/main.js
@@ -4,20 +4,30 @@ btnCreate.addEventListener("click",createNewGame);
 
 function createNewGame(){
   var container = document.getElementById('container');
-  var background = document.createElement("section");
-  var snake = document.createElement("div");
+  var backgroundSnake = document.getElementById("edge");
+  var snake = document.getElementById("snake");
+  var tamanioGame = tamanioTabla();
 
-  background.style.width = "50px";
-  background.style.height = "50px";
-  background.style.backgroundColor = "green";
-  background.style.position = "absolute";
+  tamanioGame = tamanioGame*10;
 
-  snake.style.width = "9px";
-  snake.style.height = "9px";
+  backgroundSnake.style.width = tamanioGame + "px";
+  backgroundSnake.style.height = tamanioGame + "px";
+  backgroundSnake.style.backgroundColor = "green";
+  backgroundSnake.style.position = "absolute";
+
+
+  snake.style.width = "10px";
+  snake.style.height = "10px";
   snake.style.backgroundColor = "black";
   snake.style.position = "absolute";
 
-  container.appendChild(background);
+  container.appendChild(backgroundSnake);
   container.appendChild(snake);
+
+}
+
+function tamanioTabla(){
+  var tamanio = document.getElementById('tamanio').value;
+  return tamanio;
 
 }
